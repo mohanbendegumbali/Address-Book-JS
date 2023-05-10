62eb6bbfaf5da05487cb2f493f8339d37cebefef
mohanbendegumbali <mohan0311@hotmail.com>
2023-05-10 20:47:13 +0530

    Create Contact

---
+++ address.js
@@ -0,0 +1,19 @@
+class AddressBook {
+    constructor(...params) {
+        this.firstName = params[0];
+        this.lastName = params[1];
+        this.address = params[2];
+        this.city = params[3];
+        this.state = params[4];
+        this.zip = params[5];
+        this.phoneNumber = params[6];
+        this.email = params[7];
+    }
+
+    toString() {
+        return "First Name : " + this.firstName + ", Last Name : " + this.lastName + ", Address : " + this.address + ", City : " + this.city + ", State : " + this.state + ", Zip : " + this.zip + ", Phone Number : " + this.phoneNumber + ", Email : " + this.email;
+    }
+}
+
+let addressBook = new AddressBook("Mohan", "B", "Vijaynagar", "Bangalore", "Karnataka", "411057", "93839294848", "mohan@gmail.com");
+console.log(addressBook.toString());
